972df134e69c83ca0ba7819feb56b0ba79312c47
Goosie <perry.smit@gmail.com>
2025-12-03 15:45:16 +0100

    changed main.tsx router basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles/theme.css'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/NostrQuizAndVote">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
